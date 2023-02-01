9aa64d3a37ad0c5769edef92d89855c117f158cc
youssef <youssefzekry97@gmail.com>
2023-02-01 23:07:29 +0200

    created core function for submitting users

---
+++ src/app/models/user.ts
@@ -0,0 +1,5 @@
+export class User{
+	firstName:string = "youssef";
+	lastName:string = "zekry";
+	age:number = 26;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
+  users: User[] = [];
 
+
+
+  AddUserValidation(newFirstName: string ,newLastName: string,newAge:string): void{
+
+     let user = new User();
+      user.firstName = newFirstName;
+      user.lastName = newLastName;
+      user.age = parseInt(newAge);
+      this.users.push(user);
+      console.log(this.users)
+   
+    // if (
+
+
+    // )
+    //   return false;
+
+    // else return true;
+  }
+
+  
 }
