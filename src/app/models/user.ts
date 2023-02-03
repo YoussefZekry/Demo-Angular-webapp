084d82527ea111323d6b63a946254cfee9ef3789
youssef <youssefzekry97@gmail.com>
2023-02-03 06:04:14 +0200

    added constructor for "User"

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,4 +2,11 @@ export class User{
 	firstName:string = "youssef";
 	lastName:string = "zekry";
 	age:number = 26;
+
+	constructor(firstName :string,lastName: string,age: number)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.age=age;
+	}
 }
