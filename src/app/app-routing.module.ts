7ad5279f001d9383802c51500bf79da549d5763f
youssef <youssefzekry97@gmail.com>
2023-02-04 09:18:25 +0200

    add routing& new,edit student ,notfound components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewStudentComponent } from './components/new-student/new-student.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { StudentComponent } from './components/student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:StudentComponent},
+  {path:'addstudent',component:NewStudentComponent},
+  {path:'studentdetails/:id',component:StudentDetailsComponent},
+  {path:'',component:StudentComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './components/student/student.component';
+import { NewStudentComponent } from './components/new-student/new-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    NewStudentComponent,
+    EditStudentComponent,
+    StudentDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
